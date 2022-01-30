bd722587f0edd026e1fcaaa48df81641ad45de62
AmasiaNalbandian <amasia.nalbandian@mitel.com>
2022-01-30 18:55:47 -0500

    fixed tests to pass run

--- src/authorization/cognito.js
+++ src/authorization/cognito.js
@@ -1,4 +1,4 @@
-// src/authentication.js
+// src/authorization/cognito.js
 
 // Configure a JWT token strategy for Passport based on
 // Identity Token provided by Cognito. The token will be
@@ -9,6 +9,7 @@ const BearerStrategy = require('passport-http-bearer').Strategy;
 const { CognitoJwtVerifier } = require('aws-jwt-verify');
 
 const logger = require('../logger');
+require('dotenv').config();
 
 // We expect AWS_COGNITO_POOL_ID and AWS_COGNITO_CLIENT_ID to be defined.
 if (!(process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID)) {
@@ -19,7 +20,6 @@ if (!(process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID)) {
 // get from a user is valid and something we can trust. See:
 // https://github.com/awslabs/aws-jwt-verify#cognitojwtverifier-verify-parameters
 const jwtVerifier = CognitoJwtVerifier.create({
-  // These variables must be set in the .env
   userPoolId: process.env.AWS_COGNITO_POOL_ID,
   clientId: process.env.AWS_COGNITO_CLIENT_ID,
   // We expect an Identity Token (vs. Access Token)
