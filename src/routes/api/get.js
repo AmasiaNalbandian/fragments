b711a63e5103254dda9f0176fdd686aedbc77fe5
AmasiaNalbandian <amasia.nalbandian@mitel.com>
2022-02-13 20:18:27 -0500

    added get route

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,15 +1,36 @@
 // src/routes/api/get.js
 // Our response handlers
 const response = require('../../response');
+const { Fragment } = require('../../model/fragment');
+const logger = require('../../logger');
 /**
  * Get a list of fragments for the current user
  */
 module.exports = (req, res) => {
-  // TODO: this is just a placeholder to get something working
-  res.status(200).json(
-    response.createSuccessResponse({
-      status: 'ok',
-      fragments: [],
+  async function getFragments() {
+    logger.info(`API - get.js: Attempting to get fragments by user`);
+    var fragments = await Fragment.byUser(req.user, req.query?.expand);
+    return fragments;
+  }
+  logger.debug(`GET v1/fragments - Query: ${req.query.expand}`);
+  getFragments()
+    .then((fragments) => {
+      res.status(200).json(
+        response.createSuccessResponse({
+          status: 'ok',
+          fragments: fragments,
+        })
+      );
     })
-  );
+    .catch((e) => {
+      res.status(400).json(
+        response.createErrorResponse({
+          status: 'error',
+          error: {
+            message: 'Something went wrong trying to get fragments for user by id',
+            code: 404,
+          },
+        })
+      );
+    });
 };
